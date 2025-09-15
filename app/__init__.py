fd47d07909ecd638bed25ba2928c498ff4130bad
osokhneva-debug <o.sokhneva@gmail.com>
2025-09-15 15:19:14 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 EmoJournal Telegram Bot
-Emotion tracking with scientific approach and random scheduling
+Emotion tracking with scientific approach and fixed scheduling
 """
 
 __version__ = "1.0.0"
@@ -11,7 +11,7 @@ __description__ = "Telegram bot for emotion tracking and weekly insights"
 # Make main modules easily importable
 from .main import EmoJournalBot
 from .db import Database, User, Entry, Schedule
-from .scheduler import RandomScheduler
+from .scheduler import FixedScheduler
 from .analysis import WeeklyAnalyzer
 from .i18n import Texts
 
@@ -21,7 +21,7 @@ __all__ = [
     'User', 
     'Entry',
     'Schedule',
-    'RandomScheduler',
+    'FixedScheduler',
     'WeeklyAnalyzer', 
     'Texts'
 ]
